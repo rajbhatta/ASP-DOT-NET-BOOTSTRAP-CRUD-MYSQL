3ac4bbeb9f955d1d70e82e11cdee395522f24106
Raj Bhatta <bhattaraj21@gmail.com>
2021-01-24 12:08:26 -0800

    Master page setup

--- CrudApplication/CrudApplication/Crud.Master.cs
+++ CrudApplication/CrudApplication/Crud.Master.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace CrudApplication
 {
-    public partial class Default : System.Web.UI.Page
+    public partial class Crud : System.Web.UI.MasterPage
     {
         protected void Page_Load(object sender, EventArgs e)
         {
