2c9eccb963178878dce7ae906abc121620450c40
Raj Bhatta <bhattaraj21@gmail.com>
2021-01-24 13:02:29 -0800

    Header, footer common for all pages

--- CrudApplication/CrudApplication/Crud.Master.designer.cs
+++ CrudApplication/CrudApplication/Crud.Master.designer.cs
@@ -31,12 +31,12 @@ namespace CrudApplication {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
-        /// ContentPlaceHolder1 control.
+        /// bodyContentPlaceHolder control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder bodyContentPlaceHolder;
     }
 }
---
+++ CrudApplication/CrudApplication/WebForm1.aspx
@@ -0,0 +1,8 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Crud.Master" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="CrudApplication.WebForm1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="bodyContentPlaceHolder" runat="server">
+    <h1>This is our content page</h1>
+</asp:Content>
