20fb23d8fe52912ba9cd8404b31db21888c7bd3f
Raj Bhatta <bhattaraj21@gmail.com>
2021-01-24 14:58:19 -0800

    DB environment is setup for multiple databases

---
+++ CrudApplication/CrudApplication/Services/DbEnvironment/OracleDbPropertyService.cs
@@ -0,0 +1,35 @@
+namespace CrudApplication.Services.DbEnvironment
+{
+    public class OracleDbPropertyService:IDbPropertyService
+    {
+        public string GetBaseUrl()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string GetUserName()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string GetPassword()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string GetDatabaseName()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string GetProviderName()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string GetCompleteMySqlConnectionString()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
